a715c7080ec0dc3c5ed70cfad9791106d6fbd40e
sidju <david.m.o@telia.com>
2018-12-04 09:24:08 +0100

    Initial codebase.

---
+++ make.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+javac src/CollisionDetect.java -d out
---
+++ src/CollisionDetect.java
@@ -0,0 +1,7 @@
+public class CollisionDetect
+{
+    public static void main(String[] args)
+    {
+        System.out.println("CollisionDetect");
+    }
+}
