20646ca37b445f03188a393980f2ce716e52c29f
sidju <david.m.o@telia.com>
2018-12-11 09:57:41 +0100

    concept prototype added

---
+++ prototype.sh
@@ -0,0 +1,5 @@
+while true
+      do
+	  echo '{ "itemId": "103636965585520737", "stationId": "1", "appId": "123", "typeId": "OSCAR_BLACKBOARD_TYPE_VEHICLE_STATUS", "location": { "latitude": 52.5228688988356, "longitude": 13.410204655793073 }, "locationConfidence": 0.8, "validityDuration": 1.5, "validityArea": "NaN", "blackboardProperties": [ { "type": "OSCAR_BLACKBOARD_PROPERTY_ACCELERATION", "acceleration": 0 }, { "type": "OSCAR_BLACKBOARD_PROPERTY_SPEED", "speed": 0 }, { "type": "OSCAR_BLACKBOARD_PROPERTY_YAW_RATE", "yawRate": 0 } ] }'
+	  sleep 0.5
+done
