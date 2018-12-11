792b38a9bf37d20b8568fca32f7282ebffbe9234
sidju <david.m.o@telia.com>
2018-12-11 09:26:27 +0100

    Database designed in database.sql

---
+++ database.sql
@@ -0,0 +1,22 @@
+drop owned by v2x;
+
+CREATE TABLE blackboard (
+       key SERIAL PRIMARY KEY,
+       itemid INTEGER,
+       stationid INTEGER,
+       appid INTEGER,
+       typeid VARCHAR,
+       latitude FLOAT,
+       longitude FLOAT,
+       locationconfidence INTEGER,
+       validityduration float,
+       validityarea INTEGER --Currently unused--
+);
+
+CREATE TABLE properties (
+       parent INTEGER,
+       type VARCHAR,
+       value INTEGER,
+
+       FOREIGN KEY (parent) REFERENCES blackboard
+);
